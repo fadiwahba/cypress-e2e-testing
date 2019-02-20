f25dfcc30a47654afecb4bda9d52d688cc968748
Fady Soliman <Fady.Soliman@valocity.co.nz>
2019-02-20 14:32:35 +1300

    add npm test script

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -6,14 +6,25 @@ describe("Form test", function() {
     cy.wait(1000);
   });
 
-  it( 'can submit a form when all required fields are not empty', function() {
-    cy.get("#validationDefault01").type("John");
-    cy.get("#validationDefault02").type("Doe");
-    cy.get("#validationDefaultUsername").type("john_doe");
-    cy.get("#validationDefault03").type("Auckland");
-    cy.get("#validationDefault04").type("Auckland");
-    cy.get("#validationDefault05").type("1106");
-    cy.get("#invalidCheck2").click();
+  it( 'can submit a form when all required fields are NOT empty', function() {
+    cy.get("#validationCustom01").type("John");
+    cy.get("#validationCustom02").type("Doe");
+    cy.get("#validationCustomUsername").type("john_doe");
+    cy.get("#validationCustom03").type("Auckland");
+    cy.get("#validationCustom04").type("Auckland");
+    cy.get("#validationCustom05").type("1106");
+    cy.get("#invalidCheck").click();
+    cy.get(".js-submit").click();
+  })
+
+  it( 'cannot submit a form when all required fields are empty', function() {
+    // cy.get("#validationDefault01").type("John");
+    // cy.get("#validationDefault02").type("Doe");
+    // cy.get("#validationDefaultUsername").type("john_doe");
+    // cy.get("#validationDefault03").type("Auckland");
+    // cy.get("#validationDefault04").type("Auckland");
+    // cy.get("#validationDefault05").type("1106");
+    // cy.get("#invalidCheck2").click();
     cy.get(".js-submit").click();
   })
 });
